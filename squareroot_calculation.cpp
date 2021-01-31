a5011c9aa38f1a5383d2bd5f3139f8bd8777adfb
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-01-31 11:26:02 +0530

    --

---
+++ squareroot_calculation.cpp
@@ -0,0 +1,18 @@
+//***********CALCULATE SQUARE ROOT************
+
+
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	cout<<"square root of 4 ->"<<ends<<sqrt(4)<<endl;
+	cout <<"square root of 8->"<<ends<<sqrt(8)<<endl;
+	cout<<"square root of 36->"<<ends<<sqrt(36)<<endl;
+	cout<<"square root of 68->"<<ends<<sqrt(68)<<endl;
+	
+	return 0;
+}
