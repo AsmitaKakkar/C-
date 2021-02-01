276d15d16c67d07883c2fb954139f571b2929a36
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-02-02 01:42:38 +0530

    --

---
+++ powe.cpp
@@ -0,0 +1,19 @@
+//*************CALULATE POWER***********
+
+
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+	cout<<"2^2="<<ends;
+	cout<<pow(2,2)<<endl;
+	cout<<"2^3="<<ends<<pow(2,3)<<endl;
+	cout<<"2^4="<<ends<<pow(2,4)<<endl;
+	cout<<"2^5="<<ends<<pow(2,5)<<endl;
+	
+	return 0;
+}
