acf990aa2c95ed57662b1e05c0f3268fedda33c7
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-02-03 15:30:16 +0530

    --

---
+++ sizeof.cpp
@@ -0,0 +1,23 @@
+//****************TO FIND THE SIZE OF DATATYPES****************
+
+
+#include<iostream>
+#include<iomanip>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int a,b,c,d;
+	cout<<"Size of int is ->"<<ends<<sizeof(int)<<endl;
+	cout<<"Size of float is ->"<<ends<<sizeof(float)<<endl;
+	cout<<"Size of double is ->"<<ends<<sizeof(double)<<endl;
+	cout<<"Size of long is ->"<<ends<<sizeof(long)<<endl;
+	cout<<"Size of char is ->"<<ends<<sizeof(char)<<endl;
+	cout<<"Size of long double ->"<<ends<<sizeof(long double)<<endl;
+	
+	return 0;
+}
