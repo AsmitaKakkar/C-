3708c3beb304b65d2d736725f0e04b7afd109302
Asmita Kakkar <asmitakakkar11@gmail.com>
2021-02-04 01:14:44 +0530

    --

---
+++ add_no.cpp
@@ -0,0 +1,13 @@
+//*******ADDING TWO NUMBERS********
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter two numbers:";
+	cin>>a>>b;
+	cout<<"SUM="<<a+b;
+	return 0;
+}
